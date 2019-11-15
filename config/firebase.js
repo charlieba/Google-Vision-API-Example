95fc4f3f526487ac546b9f6d5e73af3a781aff26
Carlos Barillas <giovanib07@gmail.com>
2019-11-15 12:33:36 -0600

    refactor: remove keys

--- config/firebase.js
+++ config/firebase.js
@@ -2,12 +2,12 @@
 import * as firebase from 'firebase';
 
 firebase.initializeApp({  
-  apiKey: 'AIzaSyAXPq_JzkD5XZmUcde2yhLUzHJGlPSJPtA',
-  authDomain: 'detect-text-66e39.firebaseapp.com',
-  databaseURL: 'https://detect-text-66e39.firebaseio.com',
-  projectId: 'detect-text-66e39',
-  storageBucket: 'detect-text-66e39.appspot.com',
-  messagingSenderId:  '668334676189',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId:  '',
 });
 
 export default firebase;  
